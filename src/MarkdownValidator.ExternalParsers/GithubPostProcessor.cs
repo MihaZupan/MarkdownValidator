e2d97e68a197ee0df3a6c1418c6a01213297ca13
Miha Zupan <mihazupan.zupan1@gmail.com>
2018-10-02 22:29:03 +0200

    Initial commit for GithubPostProcessor

---
+++ src/MarkdownValidator.ExternalParsers/GithubPostProcessor.cs
@@ -0,0 +1,29 @@
+﻿/*
+    Copyright (c) Miha Zupan. All rights reserved.
+    This file is a part of the Markdown Validator project
+    It is licensed under the Simplified BSD License (BSD 2-clause).
+    For more information visit:
+    https://github.com/MihaZupan/MarkdownValidator/blob/master/LICENSE
+*/
+using MihaZupan.MarkdownValidator.Configuration;
+using MihaZupan.MarkdownValidator.Parsing.ExternalUrls;
+
+namespace MihaZupan.MarkdownValidator.ExternalParsers
+{
+    public class GithubPostProcessor : IUrlPostProcessor
+    {
+        public string Identifier => nameof(GithubPostProcessor);
+
+        public string[] Hostnames => new[] { "github.com" };
+
+        public void Initialize(Config configuration)
+        {
+            configuration.WebIO.RegisterDownloadableContent("raw.githubusercontent.com", "text/plain", true);
+        }
+
+        public void Process(UrlPostProcessorContext context)
+        {
+
+        }
+    }
+}
