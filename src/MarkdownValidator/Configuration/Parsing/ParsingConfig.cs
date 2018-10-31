5d320d4ab55752cf855f02b4d7b4cc25dc80f284
Miha Zupan <mihazupan.zupan1@gmail.com>
2018-10-31 17:46:10 +0100

    Add Year to copyright notice

--- src/MarkdownValidator/Configuration/Parsing/ParsingConfig.cs
+++ src/MarkdownValidator/Configuration/Parsing/ParsingConfig.cs
@@ -1,5 +1,5 @@
 ﻿/*
-    Copyright (c) Miha Zupan. All rights reserved.
+    Copyright (c) 2018 Miha Zupan. All rights reserved.
     This file is a part of the Markdown Validator project
     It is licensed under the Simplified BSD License (BSD 2-clause).
     For more information visit:
